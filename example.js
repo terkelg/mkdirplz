5dda342863b1b8e3dd26ea636d3635eab181820e
Terkel Gjervig <terkel@terkel.com>
2018-02-20 21:26:18 -0800

    formatting

--- example.js
+++ example.js
@@ -1,15 +1,13 @@
 const { join } = require('path');
 const mkdirplz = require('./src');
 
-const dirpath = join('hello', 'world', 'sync' );
+const dirpath = join('hello', 'world', 'sync');
 const filepath = join('hello', 'world', 'async', 'file.js');
 
-(async function(){
-
-    await mkdirplz('');
-    await mkdirplz('./');
-    await mkdirplz('/');
-    await mkdirplz(dirpath);
-    await mkdirplz(filepath, { filepath: true });
-
+(async function() {
+  await mkdirplz('');
+  await mkdirplz('./');
+  await mkdirplz('/');
+  await mkdirplz(dirpath);
+  await mkdirplz(filepath, { filepath: true });
 })();
